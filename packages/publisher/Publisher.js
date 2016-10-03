739d7c382a15c59dfcdd31f46fad2145b995e696
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-10-02 22:53:14 -0400

    sync texture + sens-public

--- packages/publisher/Publisher.js
+++ packages/publisher/Publisher.js
@@ -1,62 +1,57 @@
-import { Overlay, TOC } from 'substance'
+import { ProseEditorOverlayTools, TOC } from 'substance'
 import AbstractWriter from '../common/AbstractWriter'
 import PublisherTOCProvider from './PublisherTOCProvider'
 
-function PublisherWriter() {
-  PublisherWriter.super.apply(this, arguments);
-}
-
-PublisherWriter.Prototype = function() {
-
-  this.render = function($$) {
-    var SplitPane = this.getComponent('split-pane');
-    var el = $$('div').addClass('sc-publisher');
+class Publisher extends AbstractWriter {
+  render($$) {
+    let SplitPane = this.componentRegistry.get('split-pane')
+    let el = $$('div').addClass('sc-publisher')
     el.append(
       $$(SplitPane, {splitType: 'vertical', sizeB: '400px'}).append(
         this._renderMainSection($$),
         this._renderContextSection($$)
       )
-    );
-    return el;
-  };
+    )
+    return el
+  }
 
-  this._renderContextSection = function($$) {
+  _renderContextSection($$) {
     return $$('div').addClass('se-context-section').append(
       $$(TOC)
-    );
-  };
+    )
+  }
 
-  this._renderMainSection = function($$) {
-    var SplitPane = this.getComponent('split-pane');
-    var mainSection = $$('div').addClass('se-main-section');
-    var splitPane = $$(SplitPane, {splitType: 'horizontal'}).append(
+  _renderMainSection($$) {
+    let SplitPane = this.componentRegistry.get('split-pane')
+    let mainSection = $$('div').addClass('se-main-section')
+    let splitPane = $$(SplitPane, {splitType: 'horizontal'}).append(
       this._renderToolbar($$),
       this._renderContentPanel($$)
     );
-    mainSection.append(splitPane);
-    return mainSection;
-  };
+    mainSection.append(splitPane)
+    return mainSection
+  }
 
-  this._renderContentPanel = function($$) {
-    var doc = this.documentSession.getDocument();
-    var Layout = this.getComponent('layout');
-    var ScrollPane = this.getComponent('scroll-pane');
+  _renderContentPanel($$) {
+    let doc = this.documentSession.getDocument()
+    let Layout = this.componentRegistry.get('layout')
+    let ScrollPane = this.componentRegistry.get('scroll-pane')
 
-    var contentPanel = $$(ScrollPane, {
+    let contentPanel = $$(ScrollPane, {
       tocProvider: this.tocProvider,
       scrollbarType: 'substance',
       scrollbarPosition: 'left',
-      overlay: Overlay,
+      overlay: ProseEditorOverlayTools,
       highlights: this.contentHighlights
-    }).ref('contentPanel');
+    }).ref('contentPanel')
 
-    var layout = $$(Layout, {
+    let layout = $$(Layout, {
       width: 'large'
-    });
+    })
 
-    var ArticleComponent = this.componentRegistry.get('article');
+    let ArticleComponent = this.componentRegistry.get('article')
 
-    var article = doc.get('article');
+    let article = doc.get('article')
     layout.append(
       $$(ArticleComponent, {
         node: article,
@@ -64,26 +59,22 @@ PublisherWriter.Prototype = function() {
         disabled: this.props.disabled,
         configurator: this.props.configurator
       })
-    );
+    )
+    contentPanel.append(layout)
+    return contentPanel
+  }
 
-    contentPanel.append(layout);
-    return contentPanel;
-  };
+  _scrollTo(nodeId) {
+    this.refs.contentPanel.scrollTo(nodeId)
+  }
 
-  this._scrollTo = function(nodeId) {
-    this.refs.contentPanel.scrollTo(nodeId);
-  };
+  _getExporter() {
+    return this.props.configurator.createExporter('jats')
+  }
 
-  this._getExporter = function() {
-    return this.props.configurator.createExporter('jats');
-  };
+  _getTOCProvider() {
+    return new PublisherTOCProvider(this.documentSession)
+  }
+}
 
-  this._getTOCProvider = function() {
-    return new PublisherTOCProvider(this.documentSession);
-  };
-
-};
-
-AbstractWriter.extend(PublisherWriter);
-
-export default PublisherWriter;
+export default Publisher
